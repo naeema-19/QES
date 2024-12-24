f2ae2bc2fa8356c14afbba1bfa0dd1fbb2623e8f
naeema-19 <naeemaziyad9@gmail.com>
2024-12-24 16:58:17 +0400

    1st

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
           <div className="row g-5">
             <div className="col-xl-12">
               <div className="mb-5">
-                <div className="text-center row g-4 justify-center">
+                <div className="text-center row g-4">
                   <div className="col-md-12 col-lg-12 col-xl-12">
                     <div className="footer-item">
                       <a href="index.html" className="p-0">
@@ -20,10 +20,9 @@ export default function Footer() {
                         {/* <Image src="/assets/img/logo.png" alt="Logo" width={50} height={50} /> */}
                       </a>
                       <p className='text-white' >
-                        Dolor amet sit justo amet elitr clita ipsum elitr est.Lorem
-                        ipsum dolor sit amet, consectetur adipiscing...
+                      Local Expertise, Global Experience
                       </p>
-                      <div className="footer-btn d-flex">
+                      <div className="footer-btn d-flex justify-content-center align-items-center">
                         <a className="btn btn-md-square rounded-circle me-3" href="#">
                           <i className="fab fa-facebook-f" />
                         </a>
@@ -52,7 +51,7 @@ export default function Footer() {
                           </div>
                           <div>
                             <h4 className="text-white">Address</h4>
-                            <p className="text-white mb-0">123 Street New York.USA</p>
+                            <p className="text-white mb-0">Bait Al Reem Building, <br></br>1st Floor, Al Khuwair, Ministry St <br></br>Muscat – Sultanate of Oman</p>
                           </div>
                         </div>
                       </div>
@@ -73,8 +72,10 @@ export default function Footer() {
                             <i className="fa fa-phone-alt fa-2x" />
                           </div>
                           <div>
-                            <h4 className="text-white">Telephone</h4>
-                            <p className="text-white mb-0">(+012) 3456 7890</p>
+                            <h4 className="text-white">Contact</h4>
+                            <p className="text-white mb-0">Tel: +968 2495 8606,
+                            <br></br>Fax: +968 2495 8966<br></br>Direct Contact:<br></br>
+                            Abdullah Al Fadhli / +968 97747691</p>
                           </div>
                         </div>
                       </div>
